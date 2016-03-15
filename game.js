2b9fc08bacd6216b0a9891394c43087cff73f1d8
Jerry Taylor <jerry@jerrytaylorcreative.com>
2016-03-14 23:26:36 -0700

    game.js in progress. Copied questionOne code into two through four. Having a problem with an undefined in question one. Need to fix it.

--- game.js
+++ game.js
@@ -3,7 +3,7 @@
   var questionTwo = "no";//Did I go to UofO?
   var questionThree = "yes";//Am I a Photographer?
   var questionFour = "no";//Was I in the Air Force?
-  var questionFive = "no";//Do I have a son?
+  //var questionFive = "no";//Do I have a son?
   var guessesCorrect = 0;
   console.log("start");
   console.log(guessesCorrect);
@@ -25,64 +25,41 @@
   alert(message);
   console.log(guessesCorrect);
   //QuestionTwo
-  console.log("Ask the user where I went to school.");
-  var guess = prompt("Did I go to College at UofO?");
-  if (questionTwo == guess.toLowerCase()) {
-    guessIsCorrect += 1;
-  }
-  console.log("Compare the guess to ours.")
   var message = "You picked "+guess+".";
-  if (guessIsCorrect) {
-    message += " That's right, "+yourName+"! Let's try another question.";
-  }else {
-    message += " That's right, I went to PSU. Here's another question for you.";
-  }
-  alert(message);
-  console.log(guessIsCorrect);
-  //QuestionThree
-  console.log("Ask the user what my job is.");
-  var guess = prompt("Am I a photographer?");
-  if (questionThree == guess.toLowerCase()) {
-    guessIsCorrect += 1;
-  }
-  console.log("Compare the guess to ours.")
-  var message = "You picked "+guess+".";
-  if (guessIsCorrect) {
-    message += ", that's right, "+yourName+"! Good show!";
-  }else {
-    message += " Sorry, I am a photographer.";
-  }
-  alert(message);
-  console.log(guessIsCorrect);
+  var guess = prompt("Did I go to UofO?");
+if (questionTwo == guess.toLowerCase()) {
+  guessesCorrect += 1;
+  message += " That's right, "+yourName+", I didn't go to UofO, I went to PSU.";
+}else {
+  message += " I'm sorry, that's not correct, I went to PSU.";
+}
+console.log("Compare the guess to ours.")
+alert(message);
+console.log(guessesCorrect);
+//QuestionThree
+var message = "You picked "+guess+".";
+var guess = prompt("Am I a photographer?");
+if (questionThree == guess.toLowerCase()) {
+guessesCorrect += 1;
+message += " That's right, "+yourName+", I am from Photographer! I have another question for you.";
+}else {
+message += " I'm sorry, that's not correct, I'm a photographer.";
+}
+console.log("Compare the guess to ours.")
+alert(message);
+console.log(guessesCorrect);
 //QuestionFour
-console.log("Ask the user what service I was in.");
+var message = "You picked "+guess+".";
 var guess = prompt("Was I in the Air Force?");
 if (questionFour == guess.toLowerCase()) {
-  guessIsCorrect += 1;
+guessesCorrect += 1;
+message += " That's right, "+yourName+", I wasn't! I was in the Navy.";
+}else {
+message += " I'm sorry, that's not correct, I was in the Navy.";
 }
 console.log("Compare the guess to ours.")
-var message = "You picked "+guess+".";
-if (guessIsCorrect) {
-  message += ", that's right, "+yourName+"! Let's try another question.";
-}else {
-  message += " I'm Sorry, I was in the Navy. We'll try another question!";
-}
 alert(message);
-console.log(guessIsCorrect);
-//QuestionFive
-console.log("Ask the user if I have a son.");
-var guess = prompt("Do I have a son?");
-if (questionFive == guess.toLowerCase()) {
-  guessIsCorrect += 1;
-}
-console.log("Compare the guess to ours.")
-var message = "You picked "+guess+".";
-if (guessIsCorrect) {
-  message += ", that's right, "+yourName+"! Good show!";
-}else {
-  message += " Sorry, I have two daughters. Thanks for playing!";
-}
-alert(message);
+console.log(guessesCorrect);
 //Final Question
 console.log(guessIsCorrect);
 console.log("How many questions did I get correct?");
