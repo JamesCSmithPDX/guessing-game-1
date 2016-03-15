5c19beec4a350d788f63d2ca05c953103dd78886
Jerry Taylor <jerry@jerrytaylorcreative.com>
2016-03-14 21:22:24 -0700

    first question is now correct and the counter is working.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # guessing-game
+I've created a new branch called N3-assignment and pushed it up to git hub. I've created the game.js file and moved my JavaScript to it making the link in the index.html file. With Mark's help I've implemented the toLowerCase() to change any string that's returned as an answer to lower case.
--- game.js
+++ game.js
@@ -4,29 +4,30 @@
   var questionThree = "yes";//Am I a Photographer?
   var questionFour = "no";//Was I in the Air Force?
   var questionFive = "no";//Do I have a son?
-  var guessIsCorrect = false;
+  var guessesCorrect = 0;
+  console.log("start");
+  console.log(guessesCorrect);
   //Ask their Name
   console.log("Ask the user their name.")
   var yourName = prompt("What is your name?");
   console.log("Ask the user where I'm from.");
   //QuestionOne
-  var guess = prompt("Nice to meet you, "+yourName+"! My first question for you is: am I from Portland?");
+    var message = "You picked "+guess+".";
+    var guess = prompt("Nice to meet you, "+yourName+"! My first question for you is: am I from Portland?");
   if (questionOne == guess.toLowerCase()) {
-    guessIsCorrect = true;
-  }
-  console.log("Compare the guess to ours.")
-  var message = "You picked "+guess+".";
-  if (guessIsCorrect) {
+    guessesCorrect += 1;
     message += " That's right, "+yourName+", I am from Portland! I have another question for you.";
   }else {
     message += " I'm sorry, that's not correct, I'm from Portland.";
   }
+  console.log("Compare the guess to ours.")
   alert(message);
+  console.log(guessesCorrect);
   //QuestionTwo
   console.log("Ask the user where I went to school.");
   var guess = prompt("Did I go to College at UofO?");
   if (questionTwo == guess.toLowerCase()) {
-    guessIsCorrect = false;
+    guessIsCorrect += 1;
   }
   console.log("Compare the guess to ours.")
   var message = "You picked "+guess+".";
@@ -36,11 +37,12 @@
     message += " That's right, I went to PSU. Here's another question for you.";
   }
   alert(message);
+  console.log(guessIsCorrect);
   //QuestionThree
   console.log("Ask the user what my job is.");
   var guess = prompt("Am I a photographer?");
   if (questionThree == guess.toLowerCase()) {
-    guessIsCorrect = true;
+    guessIsCorrect += 1;
   }
   console.log("Compare the guess to ours.")
   var message = "You picked "+guess+".";
@@ -50,11 +52,12 @@
     message += " Sorry, I am a photographer.";
   }
   alert(message);
+  console.log(guessIsCorrect);
 //QuestionFour
 console.log("Ask the user what service I was in.");
 var guess = prompt("Was I in the Air Force?");
 if (questionFour == guess.toLowerCase()) {
-  guessIsCorrect = true;
+  guessIsCorrect += 1;
 }
 console.log("Compare the guess to ours.")
 var message = "You picked "+guess+".";
@@ -64,11 +67,12 @@ if (guessIsCorrect) {
   message += " I'm Sorry, I was in the Navy. We'll try another question!";
 }
 alert(message);
+console.log(guessIsCorrect);
 //QuestionFive
 console.log("Ask the user if I have a son.");
 var guess = prompt("Do I have a son?");
 if (questionFive == guess.toLowerCase()) {
-  guessIsCorrect = true;
+  guessIsCorrect += 1;
 }
 console.log("Compare the guess to ours.")
 var message = "You picked "+guess+".";
@@ -76,5 +80,20 @@ if (guessIsCorrect) {
   message += ", that's right, "+yourName+"! Good show!";
 }else {
   message += " Sorry, I have two daughters. Thanks for playing!";
+}
+alert(message);
+//Final Question
+console.log(guessIsCorrect);
+console.log("How many questions did I get correct?");
+var guess = parseInt(prompt("How many questions did I get correct?"));
+if (guess == guessIsCorrect) {
+guessIsCorrect == true;
+}
+console.log("Compare the guess to ours.")
+var message = "You picked "+guess+".";
+if (guessIsCorrect) {
+  message += ", that's right, "+yourName+"! Good show!";
+}else {
+  message += " Sorry, that's not correct. Thanks for playing!";
 }
 alert(message);
